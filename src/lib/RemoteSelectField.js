cc02cba80ef2c26407cbd4e907b9d2b8b1a0f40c
Unix <mb_wali@hotmail.com>
2021-02-22 19:39:53 +0100

    bugfix: languages title as title_l10n

--- src/lib/RemoteSelectField.js
+++ src/lib/RemoteSelectField.js
@@ -1,6 +1,7 @@
 // This file is part of React-Invenio-Deposit
 // Copyright (C) 2020 CERN.
 // Copyright (C) 2020 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
 //
 // React-Invenio-Deposit is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
@@ -26,7 +27,7 @@ const encodeQuery = (query) =>
 
 const serializeSuggestions = (suggestions) =>
   suggestions.map((item) => ({
-    text: item.title,
+    text: item.title_l10n,
     value: item.id,
     key: item.id,
   }));
